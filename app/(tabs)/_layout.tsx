5e69cb4e7d1b224821bc91a273a69438bea6aa28
Nguyen, Dennis Duy Quang <dennisnguyen1475@gmail.com>
2025-07-09 13:37:25 -0400

    Changed Tabs
    Changed the tab for both maps and PinPoints(Recomendations). Remember recomendations is too long so it cuts out

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,15 +29,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'PinPoint',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="play.rectangle.on.rectangle" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="map"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="map" color={color} />,
         }}
       />
     </Tabs>
